719b44d57523d931d24c25ab3be61094c565ef0e
xara bennett-jones <xarabennettjones@gmail.com>
2022-07-19 22:45:25 +0100

    Add responsiveness

--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
@@ -1,22 +1,28 @@
 import "./DeleteWarehouseModal.scss";
 import { ButtonCancel, ButtonDelete } from "../Button/Button";
 import closeIcon from "../../assets/icons/close-24px.svg";
-
+import axios from "axios";
 import React from "react";
 
-export const DeleteWarehouseModal = () => {
+export const DeleteWarehouseModal = (props) => {
+  const warehouseId = props?.warehouse?.id || 1;
+  const deleteWarehouse = () => {
+    axios.delete(`http:localhost:8080/warehouses/${warehouseId}`);
+    // close pop up
+  };
   return (
     <section className="container">
       <img className="icon-close" alt="close icon" src={closeIcon} />
-      <h1 className="title">Delete Washington warehouse?</h1>
-      <p>
-        Please confirm that you’d like to delete the Washington from the list of
-        warehouses. You won’t be able to undo this action.
-      </p>
+      <div>
+        <h1 className="title">Delete Washington warehouse?</h1>
+        <p>
+          Please confirm that you’d like to delete the Washington from the list
+          of warehouses. You won’t be able to undo this action.
+        </p>
+      </div>
       <div className="button-container">
         <ButtonCancel />
-
-        <ButtonDelete />
+        <ButtonDelete onClick={deleteWarehouse} />
       </div>
     </section>
   );
