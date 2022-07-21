75a2140e6fc1032767996c97bdb860d94cc1c4f3
ykuruvilla <yasha.kuruvilla@gmail.com>
2022-07-21 16:02:03 +0100

    update class names

--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.js
@@ -1,7 +1,7 @@
 import "./DeleteWarehouseModal.scss";
 import Button from "../Button/Button";
 import closeIcon from "../../assets/icons/close-24px.svg";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import axios from "axios";
 import React from "react";
 
@@ -11,23 +11,23 @@ export const DeleteWarehouseModal = (props) => {
     axios.delete(`http:localhost:8080/warehouse/${warehouseId}`);
   };
   return (
-    <section className="container">
-      <Link to="/">
-        <img className="icon-close" alt="close icon" src={closeIcon} />
-      </Link>
+    <section className="delete__container">
+      <NavLink to="/" className="delete__link-close">
+        <img className="delete__icon-close" alt="close icon" src={closeIcon} />
+      </NavLink>
       <div>
-        <h1 className="title">Delete Washington warehouse?</h1>
+        <h1 className="delete__title">Delete Washington warehouse?</h1>
         <p className="body-large">
           Please confirm that you’d like to delete the Washington from the list
           of warehouses. You won’t be able to undo this action.
         </p>
       </div>
-      <div className="buttons-container">
-        <Link to="/" className="link">
-          <div className="button__margins">
+      <div className="delete__buttons-container">
+        <NavLink to="/" className="delete__cancel-link">
+          <div className="delete__button-margins">
             <Button type="cancel" />
           </div>
-        </Link>
+        </NavLink>
 
         <Button type="delete" onClick={deleteWarehouse} />
       </div>
